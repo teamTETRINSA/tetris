cc938c9f2aab35ead30847992f69bf7ec70e2ba2
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2021-03-19 16:02:57 +0100

    Add files via upload

--- tetrimino.java
+++ tetrimino.java
@@ -83,20 +83,26 @@ public class tetrimino extends shape{
         super.dessine(g);
 	}
 
-	public void rotateRotate90 (tetrimino t){
-		int [][] revTab = new int [tab.length][tab[0].length];
+	public tetrimino rotateRotate90 (tetrimino t){
+		int [][] revTab = new int [t.tab.length][t.tab[0].length];
+        
+        //the lines are moved down
         for (int j = 0; j<revTab.length; j++){
 			for (int i = 0; i<revTab[0].length; i++){
-				revTab[i][j]=tab[tab.length-1-i][j];
+				revTab[i][j]=t.tab[t.tab.length-1-i][j];
 			}
 		}
 		
 		//transpose of the matrix
-		int [][] transTab = new int [tab.length][tab[0].length];
+		int [][] transTab = new int [t.tab.length][t.tab[0].length];
 		for (int p = 0; p<transTab.length; p++){
 			for (int q = 0; q<transTab[0].length; q++){
 				transTab[p][q]=revTab[q][p];
 			}
 		}
+        
+        t.tab=revTab;
+        return t ;
+    }
 
 }
