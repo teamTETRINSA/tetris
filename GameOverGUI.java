21ea9f890d2302252e80b7f038802a0b2ae2d558
paulterrad <80112671+paulterrad@users.noreply.github.com>
2021-04-07 20:53:28 +0200

    Add files via upload

--- GameOverGUI.java
+++ GameOverGUI.java
@@ -4,6 +4,7 @@ import java.awt.event.*;
 import java.awt.geom.RoundRectangle2D;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
+import java.util.logging.*;
 
 // Sound support
 import java.io.File;
@@ -15,6 +16,8 @@ public class GameOverGUI extends JFrame {
 
 	private int score;
 	
+	private grid data;
+	
 	private JPanel gameOverPanel;
 	
 	private JPanel panelBackground;
@@ -22,10 +25,21 @@ public class GameOverGUI extends JFrame {
 	private Clip gameOverSound;
 	
 	private JButton replay;
+	
+	// Coordinates for String animation // 
+	
+	private int coordX = 0;
+    private int coordY = 60;
+    private int coordZ = 150;
+    private int coordT = 180;
+    
+    private String string1 = "! New Record !";
+    private String string2 = "Best Score";
 
-	public GameOverGUI (int scoregame) {
+	public GameOverGUI (int scoregame, grid g) {
 	
 		score = scoregame;
+		data = g;
 		
 		// Definition of the Frame //
         
@@ -37,6 +51,7 @@ public class GameOverGUI extends JFrame {
         this.setLayout(null);
         this.setShape(new RoundRectangle2D.Double(0, 0, 500, 280, 30, 30));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
         
@@ -66,12 +81,24 @@ public class GameOverGUI extends JFrame {
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 		
 		// Animated Image //
-		    
+		
+		if (score < data.bestScore) {
+		
         Icon imgIcon = new ImageIcon(this.getClass().getResource("gameover.gif"));
 		JLabel label = new JLabel(imgIcon);
 		label.setBounds(0,0,500,280); 
 		//this.getContentPane().add(label);	
 		panelBackground.add(label);
+		
+		} else if (score >= data.bestScore) {
+		
+		Icon imgIcon = new ImageIcon(this.getClass().getResource("gameoverbis.gif"));
+		JLabel label = new JLabel(imgIcon);
+		label.setBounds(0,0,500,280); 
+		//this.getContentPane().add(label);	
+		panelBackground.add(label);
+			
+		}
 		
 	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 		
@@ -94,11 +121,55 @@ public class GameOverGUI extends JFrame {
         File tetrisSoundFile = new File("gameOver.wav");		
         AudioInputStream audiotetrisSound = AudioSystem.getAudioInputStream(tetrisSoundFile);
         gameOverSound = AudioSystem.getClip();		
-        gameOverSound.open(audiotetrisSound);		
+        gameOverSound.open(audiotetrisSound);	
+        gameOverSound.start();	
          
 		}catch(Exception e){ e.printStackTrace(); }
     
      
+    }
+    
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //	
+		
+		// String animation //
+	
+     public void paint(Graphics g) {
+		 
+		if (score >= data.bestScore) {
+		
+			super.paint(g);
+        
+			Graphics2D G = (Graphics2D) g;
+			G.setFont(new Font("Times Roman", Font.BOLD, 35));     
+        
+			G.setColor(Color.white);
+			G.drawString(string1, coordX, coordY);
+			G.setColor(Color.red);
+			G.drawString(string2, coordZ, coordT);
+
+			try {
+			
+				Thread.sleep(200);
+				coordX += 20;
+            
+				if(coordX > getWidth()) {
+					coordX = 0;
+				}
+        
+			repaint();
+                
+			} 
+        
+			catch (InterruptedException ex) {
+				//JOptionPane.showMessageDialog(this, ex);
+			}
+    
+		} else if (score < data.bestScore) {
+			
+			super.paint(g);
+			
+		}
+        
     }
 	
 }
--- testGUI.java
+++ testGUI.java
@@ -27,16 +27,18 @@ public class testGUI {
 	public static void main (String[] args) {
         
             grid a = new grid ();
+            a.bestScore = 4;
 			
 			//tetrisGUI myTetrisGUI = new tetrisGUI ();
 			
 			//helpPopUp helpWindow = new helpPopUp ();
+			//helpWindow.setVisible(true);
 
 			//WelcomeGUI newWindow = new WelcomeGUI (a);
 			
 			//info1PlayerPopUp newWindowBis = new info1PlayerPopUp ();
 			
-			GameOverGUI WindowGameover = new GameOverGUI (3);
+			GameOverGUI WindowGameover = new GameOverGUI (5,a);
 			WindowGameover.setVisible(true);
 	}
 }
