13b961612ff66fac36b98f1722e6c586bdbfd64d
josephbl74 <79768976+josephbl74@users.noreply.github.com>
2021-04-20 12:08:35 +0200

    Add files via upload

--- TetrisDataBase.java
+++ TetrisDataBase.java
@@ -5,34 +5,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+* */
+import java.sql.*;
+
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/** CODE A INSERER DANS LE MAINGAME DANS UNE METHODE QUI SERA APPELLEE 
+
+/** 
+ * CODE A INSERER DANS LE MAINGAME DANS UNE METHODE QUI SERA APPELLEE 
  * DEPUIS LES GUI LORSQUE L'ON AJOUTERA UN JOUEUR OU QUE L'ON VOUDRA 
  * AFFICHER UN BESTSCORE (ON METTRA CELA EN PARAMETRE ET ON UTILISERA 
  * DES IF OU CATCH POUR CHOISIR QUELLE ACTION FAIRE DANS CETTE METHODE)
  * */
 
+/**
+ * DRIVER
+ * */
 public class TetrisDataBase{
     public static void main (String[] args){
         
         int nextID = 0;
+        String newPlayer = "John";
         
         try {
             // ouverture d'un connexion à la base
             /** A MODIFIER **/
                 Connection myCo = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/TetrisDB","tetDB_id","tetDB_pw");
+                "jdbc:mysql://localhost:3306/TetrisDB");//,"tetDB_id","tetDB_pw");
 
                 
             // addig a nex player in the database
@@ -40,26 +51,27 @@ public class TetrisDataBase{
                 
             
                 // select the next id to add
-                String query = "SELECT MAX (idPlayer) from Players WHERE playerName is not NULL;" ;
-                Statement statement = connection.createStatement();
-                ResultSet rs = statement.executeQuery(query);
-                while ( rs.next() ) {
-                    System.out.println( rs.getInt("idPlayer"));
-                    nextID = 1 + rs.getInt("idPlayer");
+                String query_1 = "SELECT MAX (idPlayer) from Players WHERE playerName is not NULL;" ;
+                Statement statement_1 = myCo.createStatement();
+                ResultSet rs_1 = statement_1.executeQuery(query_1);
+                while ( rs_1.next() ) {
+                    System.out.println( rs_1.getInt("idPlayer"));
+                    nextID = 1 + rs_1.getInt("idPlayer");
                 }
                 // Insertion
-                INSERT INTO Players VALUES (nextID/*, newPlayer*/) ; // nexPlayer est un String qui sera en parametre de la méthode das le mainGame
-                    
+                String insertData = "INSERT INTO Players"+" VALUES ("+ nextID +","+ newPlayer +")" ; // nexPlayer est un String qui sera en parametre de la méthode das le mainGame
+                statement_1.executeUpdate(insertData); 
+                System.out.println("____update executed in database");
                     
                 // verifying the modification
                     
-                String query = "SELECT * from Players WHERE idPlayer = "+nextID ;
-                Statement statement = connection.createStatement();
-                ResultSet rs = statement.executeQuery(query);
-                while ( rs.next() ) {
-                    System.out.println(" new player added (id || name)"+ rs.getInt("idPlayer") + rs.getString("playerName"));
+                String query_2 = "SELECT * from Players WHERE idPlayer = "+nextID ;
+                Statement statement_2 = myCo.createStatement();
+                ResultSet rs_2 = statement_2.executeQuery(query_2);
+                while ( rs_2.next() ) {
+                    System.out.println(" new player added (id || name)"+ rs_2.getInt("idPlayer") + rs_2.getString("playerName"));
                     //we cancel the last nextID
-                    nextID = O;
+                    nextID = 0;
                 }
             }
             
@@ -82,7 +94,7 @@ public class TetrisDataBase{
             * */
             
             // fermeture de la connexion à la base
-            connection.close() ;
+            myCo.close() ;
         }
         catch (java.sql.SQLException e) {
             System.err.println ("error db") ; // Affiche l’exception
