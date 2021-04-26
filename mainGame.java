da957dbba2468c91de351704110b6a7b131e76fa
Bich-lien <79786107+Bich-lien@users.noreply.github.com>
2021-04-26 15:59:07 +0200

    Update mainGame.java

--- mainGame.java
+++ mainGame.java
@@ -1,3 +1,6 @@
+/* This is the mainclass of our game, center of the project that drives all the other functionalities
+	It creates and calls the objects for the game to run and states the general useful methods for the rest of the game 
+*/
 import java.util.LinkedList;
 import java.util.ArrayList;
 import javax.swing.*;
